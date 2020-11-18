5a70bbcf98129ec966fe28f0cbe4ad81e8eda046
MMcClure11 <mcclme02@luther.edu>
2020-11-18 13:45:17 -0600

    adds location and hometown alias

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  
   # I want my api to be travelor.com/api/v1/resource
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :locations
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
