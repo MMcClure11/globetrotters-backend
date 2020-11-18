1bbd6136f9c1327e18b699a80bd02a7b8666f3e1
MMcClure11 <mcclme02@luther.edu>
2020-11-18 14:13:22 -0600

    adds relationships and validations to models

--- db/migrate/20201118200018_create_comments.rb
+++ db/migrate/20201118200018_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.string :content
       t.integer :user_id
-      t.ingeger :attraction_id
+      t.integer :attraction_id
 
       t.timestamps
     end
