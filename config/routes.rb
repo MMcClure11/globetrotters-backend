044b92a3f0efaac2c0e72ab5fe090b21d82157f5
MMcClure11 <mcclme02@luther.edu>
2020-11-18 14:04:42 -0600

    generates resources for attractions, comments, trips and visits

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-
   
   # I want my api to be travelor.com/api/v1/resource
   namespace :api do
     namespace :v1 do
       resources :users
       resources :locations
+      resources :attractions
+      resources :comments
+      resources :trips
+      resources :visits
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
