bc033deff6790d0f278c7908a10f1a217a020387
MMcClure11 <mcclme02@luther.edu>
2020-11-19 09:45:55 -0600

    adds session current user and fills in create action for session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-
+    @user = User.find_by(username: params[:session][:credentials][:username])
+  
+    if @user && @user.authenticate(params[:session][:credentials][:password])
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :ok
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
 
  
