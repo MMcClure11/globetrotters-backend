94d54d704240da4c45686e0496a9a9f7ce701392
MMcClure11 <mcclme02@luther.edu>
2020-11-19 14:44:38 -0600

    specifies hometown attributes for user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,13 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :name, :hometown
+
+    # belongs_to :hometown, serializer: HometownSerializer
+    attribute :hometown do |user|
+      {
+        city: user.hometown.city,
+        state: user.hometown.state,
+        country: user.hometown.country
+      }
+    end
 end
